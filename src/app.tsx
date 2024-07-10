4ef5a5a36f2a4d69029c43e65bfe8d1f7b39bc1f
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-07-10 15:06:03 -0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="w-full bg-black py-2 flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-white">Hello, world!</h1>
+    </div>
+  );
+}
